758d5fcd65ea8cec2c3e9c5cee4a3fd96eea2b58
Alex Leitch <alex.leitch@gmail.com>
2016-01-12 13:18:18 -0500

    updated to have images

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ app.config(function($stateProvider, $urlRouterProvider,
 app.controller('home', ['$http', '$scope', '$sce',
   function($http, $scope, $sce) {
 
+    // truncator
+    String.prototype.trunc = String.prototype.trunc ||
+      function(n) {
+        return (this.length > n) ? this.substr(0, n - 1) + '&hellip;' :
+          this;
+      };
+
     var api = 'https://apps.tiff.net/proxy/ems/api/live/eventcollection/',
       filtervalue = 'Canada',
       filtername = 'programmes.arrangementType',
@@ -61,26 +68,23 @@ app.controller('home', ['$http', '$scope', '$sce',
         });
 
         $sce.trustAsHtml(obj.filmpitch);
-        
+
         return obj;
       });
 
-      events = _.map(events, function(n){
+      events = _.map(events, function(n) {
         // for each event, trust the film pitch, and return
-        
+
+        n.filmhex = $sce.trustAsHtml(n.filmpitch.trunc(100));
         n.filmpitch = $sce.trustAsHtml(n.filmpitch);
         n.filmnote = $sce.trustAsHtml(n.filmnote);
 
         return n;
       })
 
-      console.log(events[14].filmpitch);
+
       // todo: Implement async here to do all the list processing for speed
       $scope.all = events;
-
-      $scope.events = _.filter(events, function(n) {
-        return n.eventtype === "EventProgram";
-      });
 
       $scope.films = _.filter(events, function(n) {
         if (n.runtime > 40) {
@@ -108,12 +112,59 @@ app.controller('home', ['$http', '$scope', '$sce',
         return n.PrimaryFestivalProgramme == "Short Cuts";
       });
 
+      $scope.otherShorts = _.filter($scope.shorts, function(n) {
+        if (n.productionCompany) {
+          var l = n.productionCompany.toLowerCase();
+          if ((l.indexOf("university") === -1) && (l.indexOf(
+              "college") === -1)) {
+            return n;
+          }
+        }
+      });
+
       $scope.talks = _.filter(events, function(n) {
         var l = n.title.toLowerCase();
         return l.indexOf("in conversation") !== -1;
       });
 
-      console.log($scope.cuts, $scope.student);
+      $scope.events = _.filter(events, function(n) {
+        var l = n.title.toLowerCase();
+        return (n.eventtype === "EventProgram" && l.indexOf(
+          "in conversation") === -1);
+      });
+
+      $scope.selectThis = function(obj) {
+        $scope.selected = obj;
+      }
+      $scope.deselectThis = function() {
+        $scope.selected = '';
+      }
+
+      $scope.selectedGroup = $scope.films;
+
+      $scope.selectGroup = function(obj) {
+        $scope.selectedTitle = obj.title;
+
+        if (obj.eventKey === "studentshorts") {
+          $scope.selectedGroup = $scope.student;
+          return;
+        }
+        if (obj.eventKey === "canadastoptenshortsp") {
+          $scope.selectedGroup = $scope.cuts;
+          return;
+        }
+        if (obj.eventKey === "canadastoptenshorts3") {
+          console.log('shorts', $scope.otherShorts);
+          var reply = _.filter($scope.shorts, function(n){
+            return !n.PrimaryFestivalProgramme;
+          })
+          $scope.selectedGroup = reply; 
+        }
+         else {
+          $scope.selectedGroup = $scope.films;
+          return;
+        }
+      }
     });
 
   }
